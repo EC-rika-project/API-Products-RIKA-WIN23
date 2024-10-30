aa04ee571180024314bb33bcc0ca612597ee1c23
Adam Ottosson <addeecsgo@gmail.com>
2024-10-30 09:41:46 +0100

    Initialize ProductService with method to retrieve products by category, including sorting and pagination logic

---
+++ Products.Api/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace Products.Api.Models;
+
+public class Product
+{
+    public string ImageUrl { get; set; } = null!;
+    public string ArticleNumber { get; set; } = null!;
+    public string Name { get; set; } = null!;
+    public decimal Price { get; set; }
+}
---
+++ Products.Api/Services/ProductService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Products.Api.DAL;
+using Products.Api.Entities;
+using Products.Api.Models;
+
+namespace Products.Api.Services;
+
+public class ProductService(DataContext dataContext)
+{
+    public async Task<ServiceResult<List<Product>>> GetProductsAsync(string category, int page = 1, int size = 10, string sortBy = "price", string sortOrder = "asc")
+    {
+        
+
+        var query = dataContext.Categories
+               .Where(c => c.Name == category)
+               .SelectMany(c => c.ProductGroups)
+               .SelectMany(pg => pg.Products)
+               .Include(p => p.ProductGroup)
+               .Include(p => p.Variations);
+    }
+}
