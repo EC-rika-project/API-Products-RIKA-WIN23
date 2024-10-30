80e3ce87543326e9f9b48a2665bcf21391a60316
Adam Ottosson <addeecsgo@gmail.com>
2024-10-30 09:41:05 +0100

    ProductEndopoints, Add endpoint to retrieve products by category with filtering, pagination

---
+++ Products.Api/Endpoints/ProductEndpoints.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Products.Api.Services;
+
+namespace Products.Api.Endpoints;
+
+public static class ProductEndpoints
+{
+    public static IEndpointRouteBuilder MapCategoryEndpoints(this IEndpointRouteBuilder builder)
+    {
+
+        builder.MapGet("/products", async ([FromQuery] string category, [FromQuery] string filter, [FromQuery] int page, [FromQuery] int size, CategoryService categoryService) =>
+        {
+            var result = await productService.GetProducts(category, filter, page, size);
+            return Results.Ok(result.Data);
+        });
+
+        return builder;
+    }
+}
