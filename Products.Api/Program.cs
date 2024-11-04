646295345878ebfda5f25f7cab160ef0dddfc06c
Chrille Jansson <chrillejansson@me.com>
2024-11-04 11:35:55 +0100

    Azure/Postgres:
    use azure postgres when deploying. using secrets from the keyvault

--- Products.Api/Program.cs
+++ Products.Api/Program.cs
@@ -1,3 +1,5 @@
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 using Microsoft.EntityFrameworkCore;
 using Products.Api.DAL;
 using Products.Api.Endpoints;
@@ -7,9 +9,25 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(x =>
+
+builder.Services.AddDbContext<DataContext>( x =>
 {
-    x.UseNpgsql(builder.Configuration["Postgres:ConnectionString"]);
+    if (builder.Environment.IsDevelopment())
+    {
+        x.UseNpgsql(builder.Configuration["Postgres:ConnectionString"]);
+    }
+    else
+    {
+        var vUri = builder.Configuration["Vault:Uri"] ?? throw new ApplicationException("We are live and the vault URI is missing");
+        var client = new SecretClient(new Uri(vUri), new DefaultAzureCredential());
+        var secretName = builder.Configuration["Vault:PostgresSecret"] ?? throw new ApplicationException("We are live and the vault postgres whatever is missing");
+        var secret = client.GetSecretAsync(secretName).GetAwaiter().GetResult();
+        if (!secret.HasValue)
+        {
+            throw new ApplicationException("We are live and the secret for our db is missing");
+        }
+        x.UseNpgsql(secret.Value.Value);
+    }
 });
 
 builder.Services.AddScoped<CategoryService>();
@@ -28,28 +46,3 @@ app.MapProductEndpoints();
 
 app.UseHttpsRedirection();
 app.Run();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
