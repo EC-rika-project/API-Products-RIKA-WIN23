29ec29336c2f516304b8129e35b86895a7e2144a
Adam Ottosson <addeecsgo@gmail.com>
2024-11-06 11:10:26 +0100

    CategoryService:
    Created a method that is creating a
    new CategoryEntity and validates
    if the name already exists no matter
    case sensitivity.

--- Products.Api/Services/CategoryService.cs
+++ Products.Api/Services/CategoryService.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Products.Api.DAL;
+using Products.Api.Entities;
 using Products.Api.Models;
+using Products.Api.Models.Requests;
 
 namespace Products.Api.Services;
 
@@ -14,5 +16,40 @@ public class CategoryService(DataContext dataContext)
             Name = x.Name,
             Description = x.Description
         }).ToList());
+    }
+
+    public async Task<ServiceResult<Category>> CreateCategoryAsync(CategoryRequest categoryRequest)
+    {
+        if (await dataContext.Categories.AnyAsync(x => x.Name.Equals(categoryRequest.Name, StringComparison.CurrentCultureIgnoreCase)))
+        {
+            return ServiceResult<Category>.Failure("Category already exists.");
+        }
+        
+        var categoryEntity = new CategoryEntity
+        {
+            Name = categoryRequest.Name,
+            Description = categoryRequest.Description
+        };
+
+
+        try
+        {
+            dataContext.Categories.Add(categoryEntity);
+            await dataContext.SaveChangesAsync();
+        }
+        catch (Exception)
+        {
+
+            // Hantera andra databasfel med ett generellt felmeddelande
+            return ServiceResult<Category>.Failure("An unexpected error occured");
+        }
+
+        var createdCategory = new Category
+        {
+            Name = categoryRequest.Name,
+            Description = categoryRequest.Description
+        };
+
+        return ServiceResult<Category>.Success(createdCategory);
     }
 }
